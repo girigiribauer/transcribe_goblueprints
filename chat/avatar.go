5f7712e65eeb159abc762dd9b29a2f5016cbb9ec
girigiribauer <girigiribauer@gmail.com>
2016-03-05 21:00:30 +0900

    chapter3.2.1.3 GravatarAvatar test green

--- chat/avatar.go
+++ chat/avatar.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"crypto/md5"
 	"errors"
+	"fmt"
+	"io"
+	"strings"
 )
 
 // ErrNoAvatarURL は Avatar インスタンスがアバターの URL を返すことができない
@@ -27,6 +31,24 @@ func (AuthAvatar) GetAvatarURL(c *client) (string, error) {
 	if url, ok := c.userData["avatar_url"]; ok {
 		if urlStr, ok := url.(string); ok {
 			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
+
+// GravatarAvatar は gravatar.com 用の Avatar です
+type GravatarAvatar struct{}
+
+// UseGravatar は GravatarAvatar のインスタンスです
+var UseGravatar GravatarAvatar
+
+// GetAvatarURL は gravatar.com 用の画像URLを返します
+func (GravatarAvatar) GetAvatarURL(c *client) (string, error) {
+	if email, ok := c.userData["email"]; ok {
+		if emailStr, ok := email.(string); ok {
+			m := md5.New()
+			io.WriteString(m, strings.ToLower(emailStr))
+			return fmt.Sprintf("//www.gravatar.com/avatar/%x", m.Sum(nil)), nil
 		}
 	}
 	return "", ErrNoAvatarURL
