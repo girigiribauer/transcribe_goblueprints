3baf06eaa975c83f53bdcccc57ef3419dfbfd897
girigiribauer <girigiribauer@gmail.com>
2016-03-16 03:03:12 +0900

    chapter5.4.2 NSQ のメッセージのやりとり

--- socialpoll/counter/main.go
+++ socialpoll/counter/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
+	"github.com/bitly/go-nsq"
 	mgo "gopkg.in/mgo.v2"
 )
 
@@ -37,4 +39,32 @@ func main() {
 		db.Close()
 	}()
 	pollData := db.DB("ballots").C("polls")
+
+	var countsLock sync.Mutex // 複数の goroutine が1つのマップに同時に読み書きしないため
+	var counts map[string]int
+
+	log.Println("NSQに接続します...")
+	q, err := nsq.NewConsumer("votes", "counter", nsq.NewConfig())
+	if err != nil {
+		fatal(err)
+		return
+	}
+
+	// NSQ からメッセージを受け取った際の処理
+	q.AddHandler(nsq.HandlerFunc(func(m *nsq.Message) error {
+		countsLock.Lock()
+		defer countsLock.Unlock()
+		if counts == nil {
+			counts = make(map[string]int)
+		}
+		vote := string(m.Body)
+		counts[vote]++
+		return nil
+	}))
+
+	// NSQ に接続
+	if err := q.ConnectToNSQLookupd("localhost:4161"); err != nil {
+		fatal(err)
+		return
+	}
 }
