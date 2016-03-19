6adbede0b7c83fd8de812481376488348cb8db7a
girigiribauer <girigiribauer@gmail.com>
2016-03-19 19:12:39 +0900

    chapter6.3.2 データベースセッションの管理

--- socialpoll/api/main.go
+++ socialpoll/api/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+
+	mgo "gopkg.in/mgo.v2"
 )
 
 func main() {
@@ -19,4 +21,13 @@ func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
 
 func isValidAPIKey(key string) bool {
 	return key == "abc123"
+}
+
+func withData(d *mgo.Session, f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		thisDb := d.Copy()
+		defer thisDb.Close()
+		SetVar(r, "db", thisDb.DB("ballots"))
+		f(w, r)
+	}
 }
